f78afed97d643009015d51dde617beb7099fa635
movieswallah <mohitrathore8463@gmail.com>
2024-12-10 23:43:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8180790178:AAEXOQSenUaywGbWWbId5snXInjfqwI
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1023211109').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/m4moviesofficial1") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002314066381'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+m4moviesofficial1')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/m4moviesofficial1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002343471142').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
